2d29fa8772cb7a023d3f9e07afce2285a992a709
Jacky Huang <lashhw81@gmail.com>
2022-10-29 15:06:35 +0800

    check closest_hit_path correctness & print statistics

--- main.cpp
+++ main.cpp
@@ -26,12 +26,61 @@ int main() {
                 r[6]
         );
 
-        bvh::SingleRayTraverser<Bvh>::Statistics statistics;
-        std::bitset<64> closest_hit_path;
+        bvh::SingleRayTraverser<Bvh>::Statistics before_statistics;
+        std::bitset<64> before_closest_hit_path;
+        auto before_result = traverser.traverse(ray, primitive_intersector, before_statistics,
+                                                false, std::bitset<64>(),
+                                                before_closest_hit_path);
+        std::cout << "Before: " << before_statistics.node_traversed << ", "
+                  << before_statistics.node_intersections << ", "
+                  << before_statistics.trig_intersections << std::endl;
+        if (before_result) {
+            std::cout << before_result->primitive_index << ", "
+                      << before_result->intersection.t << ", "
+                      << before_result->intersection.u << ", "
+                      << before_result->intersection.v << std::endl;
+        } else {
+            std::cout << "MISSED" << std::endl;
+        }
 
-        traverser.traverse(ray, primitive_intersector, statistics, closest_hit_path);
-        std::cout << "Before: " << statistics.traversal_steps << ", " << statistics.intersections << std::endl;
-        std::cout << closest_hit_path.to_string() << std::endl;
+        // check whether before_closest_hit_path is correct
+        auto first_path = before_closest_hit_path;
+        auto* curr = &bvh.nodes[0];
+        while (!curr->is_leaf()) {
+            if (first_path.test(0)) curr = &bvh.nodes[curr->first_child_or_primitive + 1];
+            else curr = &bvh.nodes[curr->first_child_or_primitive];
+            first_path = first_path >> 1;
+        }
+        if (before_result) {
+            bool found = false;
+            for (size_t i = curr->first_child_or_primitive;
+                 i < curr->first_child_or_primitive + curr->primitive_count; i++)
+                found |= bvh.primitive_indices[i] == before_result->primitive_index;
+            assert(found);
+        }
+
+        bvh::SingleRayTraverser<Bvh>::Statistics after_statistics;
+        std::bitset<64> after_closest_hit_path;
+        auto after_result = traverser.traverse(ray, primitive_intersector, after_statistics,
+                                               before_result.has_value(), before_closest_hit_path,
+                                               after_closest_hit_path);
+        std::cout << "After: " << after_statistics.node_traversed << ", "
+                  << after_statistics.node_intersections << ", "
+                  << after_statistics.trig_intersections << std::endl;
+        if (after_result) {
+            std::cout << after_result->primitive_index << ", "
+                      << after_result->intersection.t << ", "
+                      << after_result->intersection.u << ", "
+                      << after_result->intersection.v << std::endl;
+        } else {
+            std::cout << "MISSED" << std::endl;
+        }
+
+        assert(before_result->primitive_index == after_result->primitive_index);
+        assert(before_result->intersection.t == after_result->intersection.t);
+        assert(before_result->intersection.u == after_result->intersection.u);
+        assert(before_result->intersection.v == after_result->intersection.v);
+
         std::cout << std::endl;
     }
 }
